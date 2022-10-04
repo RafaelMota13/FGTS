82dc5f627fddd9fd6a6429fc330ec24da709d7e2
Rafael <faelmota7@gmail.com>
2022-10-03 22:25:24 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿decimal salario, FGTS, salLiq;
+Console.WriteLine("\n+-----------------+");
+Console.WriteLine("| CALCULADORA FGTS |");
+Console.WriteLine("+-----------------+\n");
+
+Console.Write("\nSalário (R$): ");
+salario = Convert.ToDecimal(Console.ReadLine());
+
+FGTS = salario * 8/100;
+
+salLiq = salario - FGTS;
+
+Console.WriteLine($"\nFGTS: R$ {FGTS:C2}");
+
+
+Console.Write("Salário liquido: ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+Console.WriteLine($"{salLiq:C2}\n");
+
+Console.ResetColor();
